5e02571153c1e73f066862b23d25e374f390c640
Gabor Szabo <gabor@szabgab.com>
2014-12-19 00:03:26 +0200

    some code

--- lib/Cover/App.pm
+++ lib/Cover/App.pm
@@ -4,7 +4,20 @@ use Dancer ':syntax';
 our $VERSION = '0.1';
 
 get '/' => sub {
+	my $x = rand;
+	my $y = rand;
+	if ($x < $y) {
+		my $z = add($x, $y);
+	} else {
+		my $z = add($y, $x);
+	}
     template 'index';
 };
+
+sub add {
+	my ($q, $w) = @_;
+	my $sum = $q+$w;
+	return $sum;
+}
 
 true;
